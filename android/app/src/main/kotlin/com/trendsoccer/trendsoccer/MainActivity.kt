d3009f36a24f245881aa64c841a8414e480e2dbb
unknown <trikilab2025@gmail.com>
2026-04-07 16:37:17 +0900

    Fix package declaration in MainActivity.kt to resolve build issues

--- android/app/src/main/kotlin/com/trendsoccer/trendsoccer/MainActivity.kt
+++ android/app/src/main/kotlin/com/trendsoccer/trendsoccer/MainActivity.kt
@@ -1,4 +1,8 @@
-package com.trendsoccer.trendsoccer
+git commit -m "feat: Fix theme and typography token errors
+
+- Fix fontFamilyFallback parameter in typography_tokens.dart
+- Update app_theme.dart TextTheme mapping
+- Remove auto-generated test folder"package com.trendsoccer.trendsoccer
 
 import io.flutter.embedding.android.FlutterActivity
 
